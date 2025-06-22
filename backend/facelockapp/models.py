e423a63f61902033e7c6ac24e3cd4211529c82b7
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-06-22 17:48:27 +0500

    Creating Model

--- backend/facelockapp/models.py
+++ backend/facelockapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class FaceUser(models.Model):
+    name = models.CharField(max_length=100)
+    face_image = models.models.ImageField(upload_to='faces/')
+    face_encoding = models.models.BinaryField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+    
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
 function App(){
-  return <h1 className="text-7xl" >HI Checking is Tailwind working</h1>
+return <div className="flex justify-center items-center">
+
+<h1>Lorem Ipsum Dolor Sit.</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa maiores deleniti vitae dolorum ex harum quas quo quisquam, enim, nam voluptas dolores? Exercitationem, consequatur! Quas totam tempora optio placeat cum.</p>
+
+</div>
+
 
 }
 export default App
